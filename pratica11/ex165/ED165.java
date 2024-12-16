f00436f47281aed1649f60ddd62c36931a57ea95
Isabela Cartaxo <belacartaxo@gmail.com>
2024-12-16 15:03:27 +0000

    ex165 base

---
+++ pratica11/ex165/ED165.java
@@ -0,0 +1,20 @@
+// -----------------------------------------------------------
+// Estruturas de Dados 2024/2025 (CC1007) - DCC/FCUP
+// http://www.dcc.fc.up.pt/~fds/aulas/EDados/2425/
+// -----------------------------------------------------------
+// Exemplo de utilizacao da uma arvore binaria de pesquisa
+// Ultima alteracao: 02/05/2018
+// -----------------------------------------------------------
+import java.util.Scanner;
+
+class ED165 {
+   public static void main(String[] args) {
+      BSTree<String> t = new BSTree<String>();
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+      
+      for (int i=0; i<n; i++) t.insert(in.next());
+
+      System.out.println(t.numberNodes());
+   }
+}
