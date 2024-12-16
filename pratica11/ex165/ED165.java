095ca68bd4867c9334bde7621c6a86022f4834d8
Isabela Cartaxo <belacartaxo@gmail.com>
2024-12-16 19:51:14 +0000

    ex165

--- pratica11/ex165/ED165.java
+++ pratica11/ex165/ED165.java
@@ -8,13 +8,29 @@
 import java.util.Scanner;
 
 class ED165 {
+   public static String sum(BSTree<Integer> tree, int k) {
+      return sum(tree.getRoot(), tree, k) ? "sim" : "nao";
+   }
+
+   private static boolean sum(BSTNode<Integer> n, BSTree<Integer> t, int k) {
+      if (n == null) return false;
+      if (n.getValue() > k) return sum(n.getLeft(), t, k);
+      if (t.contains(k-n.getValue())) return true;
+      return sum(n.getRight(), t, k) || sum(n.getLeft(), t, k); 
+   }
+
    public static void main(String[] args) {
-      BSTree<String> t = new BSTree<String>();
+      BSTree<Integer> t = new BSTree<Integer>();
       Scanner in = new Scanner(System.in);
       int n = in.nextInt();
-      
-      for (int i=0; i<n; i++) t.insert(in.next());
 
-      System.out.println(t.numberNodes());
+      for (int i=0; i<n; i++) t.insert(in.nextInt());
+     
+      n = in.nextInt();
+      int k;
+      for (int i=0; i<n; i++) {
+         k = in.nextInt();
+         System.out.println(k +": "+ sum(t, k));
+      }
    }
 }
